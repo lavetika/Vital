5f96894ce9df4de059ed901d4bf759ffffe6b8e0
Diana Castro <dianae.castro@gmail.com>
2021-04-12 23:51:19 -0700

    maquetado de pantallas de Alimiento y Receta

--- app/src/main/java/lopez/laura/vital/MainActivity.kt
+++ app/src/main/java/lopez/laura/vital/MainActivity.kt
@@ -1,11 +1,18 @@
 package lopez.laura.vital
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pantalla_principal)
+
+        var icono: ImageView = findViewById(R.id.icono_inicio)
+        icono.setOnClickListener {
+            startActivity(Intent(this, IniciarSesion::class.java))
+        }
     }
 }
