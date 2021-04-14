2689cb292bbb820bf1f4931e50e8be4da2fd57b8
Diana Castro <dianae.castro@gmail.com>
2021-04-13 22:47:36 -0700

    prueba navegación

--- app/src/main/java/lopez/laura/vital/Prueba.kt
+++ app/src/main/java/lopez/laura/vital/Prueba.kt
@@ -13,8 +13,9 @@ class Prueba : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prueba)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        //val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
+        /*
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -25,5 +26,7 @@ class Prueba : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        */
+
     }
 }
