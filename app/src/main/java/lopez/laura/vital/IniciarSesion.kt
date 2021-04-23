a19007e349b9b6676cc8566a6809fd95887d2981
Diana Castro <dianae.castro@gmail.com>
2021-04-22 20:50:06 -0700

    login with google acount almost done

--- app/src/main/java/lopez/laura/vital/IniciarSesion.kt
+++ app/src/main/java/lopez/laura/vital/IniciarSesion.kt
@@ -1,11 +1,21 @@
 package lopez.laura.vital
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import kotlinx.android.synthetic.main.activity_iniciar_sesion.*
+
 
 class IniciarSesion : AppCompatActivity() {
+
+    val RC_SIGN_IN = 123
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
@@ -14,5 +24,25 @@ class IniciarSesion : AppCompatActivity() {
         btnCrearCuenta.setOnClickListener {
             startActivity(Intent(this, FormularioRegistro::class.java))
         }
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        btn_google.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
     }
+
+
 }
