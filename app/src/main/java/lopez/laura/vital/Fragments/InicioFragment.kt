c00fcf13cd08b9221bd3b47ef3bb6b4239fa41ea
DanielMendoza1 <danielalejandroornelasmendoza@gmail.com>
2021-05-19 11:45:21 -0700

    Se agrega el tomar foto dentro del fragment de inicio

--- app/src/main/java/lopez/laura/vital/Fragments/InicioFragment.kt
+++ app/src/main/java/lopez/laura/vital/Fragments/InicioFragment.kt
@@ -1,14 +1,21 @@
 package lopez.laura.vital.Fragments
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.fragment_inicio.*
 import kotlinx.android.synthetic.main.fragment_inicio.view.*
-import lopez.laura.vital.AgregarAlimento
 import lopez.laura.vital.FavoritosInicio
 import lopez.laura.vital.Frutas
 import lopez.laura.vital.R
@@ -27,6 +34,10 @@ class InicioFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private val CAMERA_PERSMISSION_CODE  = 1
+    private val CAMERA_CODE = 2
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,13 +58,53 @@ class InicioFragment : Fragment() {
         }
 
         view.agregar_comida.setOnClickListener {
-            var intent  = Intent(view.context, AgregarAlimento::class.java)
-            startActivity(intent)
+            if (ContextCompat.checkSelfPermission(
+                    view.context,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_CODE)
+            } else {
+                ActivityCompat.requestPermissions(
+                    view.context as Activity,
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERSMISSION_CODE
+                )
+            }
         }
-
 
         // Inflate the layout for this fragment
         return view
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERSMISSION_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_CODE)
+            } else {
+                Toast.makeText(
+                    view!!.context,
+                    "El permiso no ha sido aceptado.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == CAMERA_CODE){
+                val image: Bitmap = data!!.extras!!.get("data") as Bitmap
+                iv_comida.setImageBitmap(image)
+            }
+        }
     }
 
     companion object {
