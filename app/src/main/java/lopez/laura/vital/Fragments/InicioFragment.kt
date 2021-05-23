0df090f2277a62332fb1bcdd51962448b4ded74e
Diana Castro <dianae.castro@gmail.com>
2021-05-23 11:17:46 -0700

    Done guardar foto en dispositivo

--- app/src/main/java/lopez/laura/vital/Fragments/InicioFragment.kt
+++ app/src/main/java/lopez/laura/vital/Fragments/InicioFragment.kt
@@ -2,10 +2,14 @@ package lopez.laura.vital.Fragments
 
 import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
@@ -17,9 +21,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.fragment_inicio.*
+import kotlinx.android.synthetic.main.fragment_inicio.view.*
 import lopez.laura.vital.FavoritosInicio
 import lopez.laura.vital.R
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -104,25 +112,55 @@ class InicioFragment : Fragment() {
         if(resultCode == Activity.RESULT_OK){
             if(requestCode == CAMERA_CODE){
                 val image: Bitmap = data!!.extras!!.get("data") as Bitmap
-                val stream = ByteArrayOutputStream()
-                image.compress(Bitmap.CompressFormat.PNG, 100, stream)
-                val byteArray: ByteArray = stream.toByteArray()
-                image.recycle()
-
-                val datosAEnviar = Bundle()
-                datosAEnviar.putByteArray("img", byteArray)
-
-                val fragmento: Fragment = InicioFragment()
-                fragmento.arguments = datosAEnviar
-                val fragmentManager: FragmentManager = activity!!.supportFragmentManager
-                val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-                fragmentTransaction.replace(R.layout.activity_agregar_comida, fragmento)
-                fragmentTransaction.addToBackStack(null)
-                fragmentTransaction.commit()
-
 
                 iv_comida.setImageBitmap(image)
+                saveMediaToStorage(image)
             }
+        }
+    }
+
+    fun saveMediaToStorage(bitmap: Bitmap) {
+        //Generating a file name
+        val filename = "${System.currentTimeMillis()}.jpg"
+
+        //Output stream
+        var fos: OutputStream? = null
+
+        //For devices running android >= Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //getting the contentResolver
+            context?.contentResolver?.also { resolver ->
+
+                //Content resolver will process the contentvalues
+                val contentValues = ContentValues().apply {
+
+                    //putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                //Inserting the contentValues to contentResolver and getting the Uri
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                //Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            //These for devices running on android < Q
+            //So I don't think an explanation is needed here
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            //Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            //context?.toast("Saved to Photos")
+            Toast.makeText(context!!, "Saved to Photos", Toast.LENGTH_SHORT).show()
         }
     }
 
